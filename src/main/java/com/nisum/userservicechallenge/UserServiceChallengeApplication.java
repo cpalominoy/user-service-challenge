169c554b1ea7ff8e4803891ee6fce644ceb45e5d
Alfredo Palomino <alfredo.palomino460@gmail.com>
2023-02-22 18:12:56 -0500

    Initial commit

---
+++ src/main/java/com/nisum/userservicechallenge/UserServiceChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.userservicechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nisum/userservicechallenge/UserServiceChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.userservicechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
